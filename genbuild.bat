83c143d98d4a335b040fb4e37d2a3a66f6677e9b
Jarl Ostensen <Jarl@polystream.com>
2020-07-23 10:28:41 +0100

    Removed gcc ASM compiler defines.

--- genbuild.bat
+++ genbuild.bat
@@ -2,13 +2,12 @@
 rem Generate the build.ninja file for all the kernel source using our local ninja.exe and LLVM/clang 10
 setlocal ENABLEDELAYEDEXPANSION
 
-PATH=%CD%\external\LLVM\bin;%CD%\external;%PATH%
+PATH=%CD%\external\cmake-3.18.0-win64-x64\bin;%CD%\external\LLVM\bin;%CD%\external;%PATH%
 
 rem https://stackoverflow.com/questions/46553436/building-with-cmake-ninja-and-clang-on-windows
 cmake -G Ninja -Bbuild^
  -DCMAKE_C_COMPILER:PATH="%CD%\external\LLVM\bin\clang.exe" -DCMAKE_C_COMPILER_ID="Clang" -DCMAKE_SYSTEM_NAME="Generic"^
- -DCMAKE_LINKER:PATH="%CD%\external\LLVM\bin\lld-link.exe"^
- -DCMAKE_ASM_COMPILER:PATH="%CD%\external\mingw64\bin\gcc.exe" -DCMAKE_ASM_COMPILER_ID="GCC"
+ -DCMAKE_LINKER:PATH="%CD%\external\LLVM\bin\lld-link.exe"
 
 endlocal
 
