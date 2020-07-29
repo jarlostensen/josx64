2126a9167222cfceb87cabec29581d1a8f7d36b1
Jarl Ostensen <Jarl@polystream.com>
2020-07-29 15:43:43 +0100

    Added support for %S (single-byte string argument) to wprintf functions.

--- lab/lab.c
+++ lab/lab.c
@@ -2,6 +2,7 @@
 // =========================================================================================
 
 #include <stdint.h>
+#include <stdio.h>
 #include <varargs.h>
 #include "../libc/internal/include/libc_internal.h"
 
@@ -9,12 +10,17 @@ extern int _JOS_LIBC_FUNC_NAME(swprintf)(wchar_t* __restrict buffer, size_t size
 extern int _JOS_LIBC_FUNC_NAME(vswprintf)(wchar_t *__restrict buffer, size_t bufsz, const wchar_t * __restrict format, va_list vlist);
 extern size_t _JOS_LIBC_FUNC_NAME(wcslen)( const wchar_t *str);
 
+//size_t _JOS_LIBC_FUNC_NAME(wcstombs)(char* __restrict dest, const wchar_t* __restrict src, size_t max)
+//{
+//	
+//}
+
 int main(void)
 {
 	wchar_t wbuffer[1024];
-	int written = _JOS_LIBC_FUNC_NAME(swprintf)(wbuffer,sizeof(wbuffer)/sizeof(wchar_t),L"Hello wide %s, %3.2f this is 0x%x == %d, and \"%c\"", L"world", 3.1459f, 0xabcdef, 42, L'J');
+	int written = _JOS_LIBC_FUNC_NAME(swprintf)(wbuffer,0,L"Hello wide %S, %3.2f this is 0x%x == %d, and \"%c\"", "world", 3.1459f, 0xabcdef, 42, L'J');	
 	wbuffer[0] = 0;
-	written = _JOS_LIBC_FUNC_NAME(swprintf)(wbuffer,0,L"Hello wide %s, %3.2f this is 0x%x == %d, and \"%c\"", L"world", 3.1459f, 0xabcdef, 42, L'J');
+	written = _JOS_LIBC_FUNC_NAME(swprintf)(wbuffer,sizeof(wbuffer)/sizeof(wchar_t),L"Hello wide %S, %3.2f this is 0x%x == %d, and \"%c\"", "world", 3.1459f, 0xabcdef, 42, L'J');
 
 	written = _JOS_LIBC_FUNC_NAME(wcslen)(0);
 	written = _JOS_LIBC_FUNC_NAME(wcslen)(L"");
