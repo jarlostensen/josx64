ff098b12c7eea3a7317454ae172972ec82f2acf9
Jarl Ostensen <jarl.ostensen@gmail.com>
2021-11-22 18:48:08 +0000

    Basic re-work of CMakelists to support "include josx64" model and added simple "int main" in Lab.

---
+++ kernel/include/josx64.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef JOSX_H_INCLUDED
+#define JOSX_H_INCLUDED
+
+#include "jos.h"
+
+// this is the main function a uKernel implements, it will be invoked 
+// when the kernel is set up and running
+extern int main(int argc, char* argv[]);
+
+#endif // JOSX_H_INCLUDED
---
+++ lab/hello_world.c
@@ -0,0 +1,7 @@
+
+#include "josx64.h"
+
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
