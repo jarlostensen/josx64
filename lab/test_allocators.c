16d4ac0b5d3ec84fe2ebc7d83042829e620bf546
Jarl Ostensen <jarl.ostensen@gmail.com>
2021-12-16 12:04:53 +0000

    Improved delete and bitscan for integer log2 calculations.

--- lab/test_allocators.c
+++ lab/test_allocators.c
@@ -15,6 +15,12 @@
 #include "../kernel/include/linear_allocator.h"
 #include "../kernel/include/arena_allocator.h"
 
+/*
+	The first bytes of each free block of pages are used to store 
+	a link to the next free block (for this order), and the "buddy tag" which 
+	is the address of the previous adjacent page xor the address of this page. 
+	This is what is used to identify pages that can be coalesced when we free allocations. 
+*/
 typedef struct _bb_page_block_header {
 
 	struct _bb_page_block_header*	_next_block;
@@ -22,20 +28,48 @@ typedef struct _bb_page_block_header {
 
 } _JOS_PACKED_ _bb_page_block_header_t;
 
+/*
+	This is a version of the Binary Buddy allocator (https://en.wikipedia.org/wiki/Buddy_memory_allocation)
+	It differs from Wikipedia's article in that it automatically shrinks and re-distributes page blocks 
+	according to the allocation request, instead of allocating a full block. 
+	This means that an allocation of 3 pages, say, will not use up an entire 4 page block but rather 
+	split it and create a new 1 free page block that will be re-inserted (in this case at order 0).
+	This can lead to fragmentation, but frees will coalesce the pages into their original form when possible (i.e. when 
+	there are no holes such that the original allocation can not be reconstructed).
+	TODO: fragmentation stress-test
+*/
 typedef struct _bb_page_allocator {
 
 	// array of _max_order entries, each pointing to 
 	// the first page in a chain of contiguous 2^order page blocks
 	uintptr_t*	_zones;
+	// the highest order for this allocator, i.e. the largest single allocation possible (in number of pages)
 	size_t		_max_order;
 
 } bb_page_allocator_t;
 
+#ifdef _JOS_KERNEL_BUILD
+static inline size_t ullog2(size_t n) {
+	return __builtin_clzll((uint32_t)n);
+}
+#else
+static inline size_t ullog2(size_t n) {
+	size_t l = 0;
+	_BitScanReverse64(&l, n);
+	return l;
+}
+#endif
+static inline size_t ceil_ullog2(size_t n) {
+	size_t d = ullog2(n);
+	// this round-up isn't very efficient...
+	return d + ((n & ((1ull << d)-1)) ? 1:0);
+}
+
 #define _BB_PAGE_ALLOC_ORDER_LO(page_count)\
-	(size_t)floorf(log2f((float)(page_count)))
+	ullog2(page_count)
 
 #define _BB_PAGE_ALLOC_ORDER_HI(page_count)\
-	(size_t)ceilf(log2f((float)(page_count)))
+	ceil_ullog2(page_count)
 
 void bb_page_allocator_create(bb_page_allocator_t* palloc, void* pool_base, size_t pool_num_pages, static_allocation_policy_t* static_allocator) {
 
@@ -67,8 +101,8 @@ void bb_page_allocator_create(bb_page_allocator_t* palloc, void* pool_base, size
 	
 	// populate zones and link buddy blocks so that we can combine them later (when things are freed)
 	uintptr_t last_page_block = 0;
-	for (int z = (int)n; z >= 0; --z) {
-		const size_t pages = (1ull << z);
+	size_t pages = (1ull << n);
+	for (int z = (int)n; z >= 0; --z) {		
 		if (pool_num_pages < pages) {
 			palloc->_zones[z] = 0;
 		}
@@ -81,6 +115,7 @@ void bb_page_allocator_create(bb_page_allocator_t* palloc, void* pool_base, size
 			aligned_pool_base += (uintptr_t)kAllocAlign_4k * pages;
 			pool_num_pages -= pages;
 		}
+		pages >>= 1;
 	}
 }
 
@@ -133,7 +168,7 @@ void* bb_page_allocator_allocate(bb_page_allocator_t* allocator, size_t page_cou
 		return NULL;
 	}
 	
-	const size_t pages_in_zone = (1ull << order);
+	
 	// the first two uintptr's of a free block of pages is used to 
 	// store the link to the next block (or 0) and the address of this block xor'd with the address of the 
 	// block immediately before it. This last part is used to determine adjacency when blocks are freed.
@@ -143,12 +178,11 @@ void* bb_page_allocator_allocate(bb_page_allocator_t* allocator, size_t page_cou
 	//
 	void* ptr = (void*)allocator->_zones[order];
 	
-
+	size_t pages_in_zone = (1ull << order);
 	_bb_page_block_header_t* next_page_block = (_bb_page_block_header_t*)allocator->_zones[order];
 	allocator->_zones[order] = (uintptr_t)next_page_block->_next_block;
 	size_t pages_to_relocate = (pages_in_zone - page_count);
-
-	printf("\torder %d allocated @ 0x%llx, %d pages left of %d\n", (int)order, (uintptr_t)ptr, (int)pages_to_relocate, (int)page_count);
+	//printf("\torder %d allocated @ 0x%llx, %d pages left of %d\n", (int)order, (uintptr_t)ptr, (int)pages_to_relocate, (int)page_count);
 
 	if (!pages_to_relocate) {
 		// we're done
@@ -158,17 +192,15 @@ void* bb_page_allocator_allocate(bb_page_allocator_t* allocator, size_t page_cou
 	uintptr_t prev_page_ptr = (uintptr_t)(ptr);
 	uintptr_t realloc_page_ptr = prev_page_ptr+(page_count * kAllocAlign_4k);
 	// distribute the remaining pages to lower zones
-	for (int z = (int)allocator->_max_order; z >= 0 && pages_to_relocate; z--) {
-		const size_t pages = (1ull << z);
-		if (pages <= pages_to_relocate) {
+	for (int z = (int)order; z >= 0 && pages_to_relocate; z--) {		
+		if (pages_in_zone <= pages_to_relocate) {
 			_bb_page_allocator_insert_pages(allocator, z, realloc_page_ptr, prev_page_ptr);
-
-			printf("\torder %d, adding 0x%llx, prev is 0x%llx\n", z, realloc_page_ptr, prev_page_ptr);
-
-			pages_to_relocate -= pages;
+			//printf("\torder %d, adding 0x%llx, prev is 0x%llx\n", z, realloc_page_ptr, prev_page_ptr);
+			pages_to_relocate -= pages_in_zone;
 			prev_page_ptr = realloc_page_ptr;
-			realloc_page_ptr += (pages * kAllocAlign_4k);
+			realloc_page_ptr += (pages_in_zone * kAllocAlign_4k);
 		}
+		pages_in_zone >>= 1;
 	}
 	return ptr;
 }
@@ -191,9 +223,9 @@ void _bb_page_allocator_free(bb_page_allocator_t* allocator, void* ptr, size_t p
 		// the original allocation split a block into sub-blocks that we now need to re-assemble (if possible)
 		uintptr_t last_page_block = (uintptr_t)ptr;
 		size_t coalesced_pages = 0ull;
+		size_t pages = (1ull << order);
 		for (int z = (int)order; z >= 0; --z) {
-			bool has_holes = false;
-			const size_t pages = (1ull << z);
+			bool has_holes = false;			
 			// this order could satisfy some of the original request
 			if (pages_relocated >= pages) {	
 				has_holes = true;
@@ -208,7 +240,7 @@ void _bb_page_allocator_free(bb_page_allocator_t* allocator, void* ptr, size_t p
 							last_page_block = (uintptr_t)page_block_header;
 							if (!prev_block_header) {
 
-								allocator->_zones[z] = 0;
+								allocator->_zones[z] = (uintptr_t)page_block_header->_next_block;
 							}
 							else {
 								prev_block_header->_next_block = page_block_header->_next_block;
@@ -230,43 +262,46 @@ void _bb_page_allocator_free(bb_page_allocator_t* allocator, void* ptr, size_t p
 			if (has_holes) {
 				break;
 			}
+
+			pages >>= 1;
 		}
 		if (last_page_block != (uintptr_t)ptr) { 
 			// we've coalesced pages; re-insert them in the topmost order from whence this allocation came...
 			// printf("\twe have coalesced %llu pages from 0x%llx to 0x%llx\n", coalesced_pages, (uintptr_t)ptr, last_page_block + (coalesced_pages * kAllocAlign_4k));
-			_bb_page_allocator_insert_pages(allocator, order, (uintptr_t)ptr, 0);			
+			_bb_page_allocator_insert_pages(allocator, order, (uintptr_t)ptr, 0);
 		}
 	}
 }
 
 static void _dump_bb_allocator(bb_page_allocator_t* allocator) {
+	printf("-------------------------------------------------------------------------\n");
 	size_t pages_available = 0;
 	for (size_t z = 0; z <= allocator->_max_order; ++z) {
 		printf("order %llu [", z);
 		if (allocator->_zones[z]) {
 			const size_t page_block_size = (1ull << z);
-			size_t pages_in_zone = 0;
+			size_t blocks_in_zone = 0;
 			uintptr_t prev_page_block = 0;
 			_bb_page_block_header_t* next_page_block = (_bb_page_block_header_t*)allocator->_zones[z];
 			do {
-				pages_in_zone += page_block_size;
+				++blocks_in_zone;
 				pages_available += page_block_size;
 				if (prev_page_block && (next_page_block->_buddy_tag ^ prev_page_block) == (uintptr_t)next_page_block) {
-					printf("x");
+					printf(">");
 				}
 				else {
-					printf(".");
+					printf("%c",178);
 				}
 				prev_page_block = (uintptr_t)next_page_block;
 				next_page_block = next_page_block->_next_block;
 			} while(next_page_block);
-			printf("] %llu pages at 0x%llx\n", pages_in_zone, allocator->_zones[z]);
+			printf("] %llu blocks, %llu pages at 0x%llx\n", blocks_in_zone, (blocks_in_zone * page_block_size), allocator->_zones[z]);
 		}
 		else {
 			printf("]\n");
 		}
 	}
-	printf("found %llu pages for a total of %llu bytes\n", pages_available, pages_available * kAllocAlign_4k);
+	printf("\t%llu pages, %llu bytes\n", pages_available, pages_available * kAllocAlign_4k);
 }
 
 void test_page_allocator(void) {
@@ -282,8 +317,11 @@ void test_page_allocator(void) {
 	void* pages = bb_page_allocator_allocate(&allocator, 13);	
 	_dump_bb_allocator(&allocator);
 	printf("\n");
-
+	void* pages2 = bb_page_allocator_allocate(&allocator, 3);
+	_dump_bb_allocator(&allocator);
 	_bb_page_allocator_free(&allocator, pages, 13);
+	_dump_bb_allocator(&allocator);
+	_bb_page_allocator_free(&allocator, pages2, 3);
 	_dump_bb_allocator(&allocator);
 	
 	free(page_pool);
