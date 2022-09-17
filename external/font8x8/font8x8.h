097db108f98c64d3444ede64f0a9f97b35a9df65
jarl.ostensen <jarl.ostensen@gmail.com>
2022-09-17 21:17:41 +0100

    Re-added external folder with non-fetched content

---
+++ external/font8x8/font8x8.h
@@ -0,0 +1,25 @@
+/** 
+ * 8x8 monochrome bitmap fonts for rendering
+ * Author: Daniel Hepper <daniel@hepper.net>
+ * 
+ * License: Public Domain
+ * 
+ * Based on:
+ * http://dimensionalrift.homelinux.net/combuster/mos3/?p=viewsource&file=/modules/gfx/font8_8.asm
+ **/
+
+#pragma once
+#ifndef _JOS_FONT_8x8_H
+#define _JOS_FONT_8x8_H
+
+#include "font8x8_basic.h"
+#include "font8x8_control.h"
+#include "font8x8_ext_latin.h"
+#include "font8x8_greek.h"
+#include "font8x8_misc.h"
+#include "font8x8_box.h"
+#include "font8x8_block.h"
+#include "font8x8_hiragana.h"
+#include "font8x8_sga.h"
+
+#endif // _JOS_FONT_8x8_H
